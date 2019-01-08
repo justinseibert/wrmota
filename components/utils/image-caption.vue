9637cd67ca23a22b57c157618e28bce142f654eb
Justin Seibert <justinseibert@users.noreply.github.com>
2019-01-07 23:19:28 -0800

    adds utils component section with img-caption

---
+++ components/utils/image-caption.vue
@@ -0,0 +1,40 @@
+<template>
+  <figure class="component__image-caption">
+    <img
+      :src="src"
+      :title="title"
+      :alt="alt"
+      />
+    <figcaption>{{ caption }}</figcaption>
+  </figure>
+</template>
+
+<script>
+  export default {
+
+    props: {
+      src: {
+        type: String,
+        default: '/img/loading.png'
+      },
+      title: {
+        type: String,
+        default: ''
+      },
+      alt: {
+        type: String,
+        default: ''
+      },
+      caption: {
+        type: String,
+        default: ''
+      },
+    },
+
+    created() {
+        this.title = this.title.length < 1 ? this.caption : this.title;
+        this.alt = this.alt.length < 1 ? this.caption : this.alt;
+    }
+
+  }
+</script>
